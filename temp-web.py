1a87a5304464d62c779e64382d7dbfbd88e961b5
Mikhail Ermakov <melhiour@gmail.com>
2019-02-03 20:58:33 +0300

    Flask restructured

--- temp-web.py
+++ temp-web.py
@@ -2,16 +2,20 @@ from flask import Flask, request, render_template
 from temp_web_modules import db_to_graph
 
 app = Flask(__name__)
-
+'''
 @app.route('/')
 def base_form():
     return render_template('index.html')
-
-@app.route('/', methods=['POST'])
+'''
+@app.route('/', methods=['GET', 'POST'])
 def graphing():
-    date_from = request.form['date_from']
-    date_until = request.form['date_until']
-    db_to_graph('/root/temp-data/temp-data.db', 'weather', '2019-02-02', '2019-02-03')
+    if request.method = 'GET':
+        return render_template('index.html')
+    else:
+        date_from = request.form['date_from']
+        date_until = request.form['date_until']
+    return(date_from, date_until)
+    #db_to_graph('/root/temp-data/temp-data.db', 'weather', '2019-02-02', '2019-02-03')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug = True)
