be485b271f4af428f31dd78fcbd00464b6a4453e
Mikhail Ermakov <melhiour@gmail.com>
2019-02-06 22:13:39 +0300

    Added graph for today app.route

--- piclock_web.py
+++ piclock_web.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+from datetime import datetime, timedelta
 from flask import Flask, request, render_template
 from piclock_web_modules import db_to_graph
 
@@ -10,6 +11,14 @@ def graphing():
     else:
         date_from = request.form['date_from']
         date_until = request.form['date_until']
+    result = db_to_graph('/root/pi_clock/temp-data.db', 'weather', date_from, date_until)
+    return result
+
+@app.route('/today')
+def graphing_today():
+    now = datetime.now()
+    date_from = now.strftime("%Y-%m-%d")
+    date_until = (now + timedelta(days=1)).strftime("%Y-%m-%d")
     result = db_to_graph('/root/pi_clock/temp-data.db', 'weather', date_from, date_until)
     return result
 
