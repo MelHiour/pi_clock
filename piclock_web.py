5c906ed4532176d61e82bcfbe57c01e210f02271
Mikhail Ermakov <melhiour@gmail.com>
2019-02-06 22:49:11 +0300

    Really disapointed about iphone's favicon...

--- piclock_web.py
+++ piclock_web.py
@@ -27,5 +27,10 @@ def graphing_today():
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                           'favicon.ico',mimetype='image/vnd.microsoft.icon')
+
+@app.route('/apple-touch-icon.png')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                          'apple-touch-icon.png',mimetype='image/vnd.microsoft.icon')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
