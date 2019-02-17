3817fc607683acbf646471024c43f4c329214e09
root <root@raspberrypi>
2019-02-17 22:26:17 +0300

    Introducing clear module which clears the display. Runs after display stop by systemd

---
+++ piclock_clear.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.virtual import viewport, sevensegment
+
+serial = spi(port=0, device=0, gpio=noop())
+device = max7219(serial, cascaded=1)
+seg = sevensegment(device)
+
+seg.text = '        '
