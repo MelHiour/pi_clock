1cb614aa29aa196030fc7b41b462e63f26e2afe1
Mikhail Ermakov <melhiour@gmail.com>
2019-02-21 21:23:52 +0300

    Changed to re.compile

--- piclock_poller.py
+++ piclock_poller.py
@@ -8,13 +8,15 @@ import Adafruit_BMP.BMP085 as BMP085
 
 sensor = BMP085.BMP085()
 shared = memcache.Client(['127.0.0.1:11211'], debug=0)
+weather_regex = re.compile(' ([-+]?\d{1,2})')
 
 while True:
     humidity, inside_temp = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 17)
     pressure = sensor.read_pressure()*0.0075006157584566
+    
     try:
         raw_weather = requests.get('http://wttr.in/?format=3')
-        outside_temp_raw = re.search(' ([-+]?\d{1,2})', raw_weather.text)
+        outside_temp_raw = weather_regex.search(raw_weather.text)
     except requests.exceptions.ConnectionError:
         outside_temp_raw = False
     finally:
