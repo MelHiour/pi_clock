647db121911b1eed0d73867e9e6a3350cb2e3718
Mikhail Ermakov <melhiour@gmail.com>
2019-03-16 23:47:31 +0300

    memcache entry has been corrected to support CCS811 sensor

--- piclock_poller.py
+++ piclock_poller.py
@@ -10,10 +10,13 @@ from Adafruit_CCS811 import Adafruit_CCS811
 sensor = BMP085.BMP085()
 shared = memcache.Client(['127.0.0.1:11211'], debug=0)
 weather_regex = re.compile(' ([-+]?\d{1,2})')
+ccs =  Adafruit_CCS811()
 
 while True:
     humidity, inside_temp = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 17)
     pressure = sensor.read_pressure()*0.0075006157584566
+    co2 = ccs.geteCO2()
+    tvoc = ccs.getTVOC()
 
     try:
         raw_weather = requests.get('http://wttr.in/?format=3')
@@ -29,7 +32,9 @@ while True:
         sensors_data = {'inside_temp': inside_temp,
                         'humidity': humidity,
                         'pressure': int(pressure),
-                        'outside_temp': outside_temp}
+                        'outside_temp': outside_temp,
+                        'co2': co2,
+                        'tvoc': tvoc}
 
         shared.set('sensors_data', sensors_data, 300)
-        time.sleep(10)
+        time.sleep(30)
