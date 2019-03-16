abd83eda4fbde73b3adfe3532dd474ec9cce8177
Mikhail Ermakov <melhiour@gmail.com>
2019-03-16 23:44:28 +0300

    Initialize CCS811 (air quality) sensor support

--- piclock_poller.py
+++ piclock_poller.py
@@ -5,6 +5,7 @@ import requests
 import memcache
 import Adafruit_DHT
 import Adafruit_BMP.BMP085 as BMP085
+from Adafruit_CCS811 import Adafruit_CCS811
 
 sensor = BMP085.BMP085()
 shared = memcache.Client(['127.0.0.1:11211'], debug=0)
@@ -13,7 +14,7 @@ weather_regex = re.compile(' ([-+]?\d{1,2})')
 while True:
     humidity, inside_temp = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 17)
     pressure = sensor.read_pressure()*0.0075006157584566
-    
+
     try:
         raw_weather = requests.get('http://wttr.in/?format=3')
         outside_temp_raw = weather_regex.search(raw_weather.text)
@@ -29,6 +30,6 @@ while True:
                         'humidity': humidity,
                         'pressure': int(pressure),
                         'outside_temp': outside_temp}
-        
+
         shared.set('sensors_data', sensors_data, 300)
         time.sleep(10)
