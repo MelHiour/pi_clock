134b153f963d863db872b5e6b97a5d15f842d6ca
root <root@raspberrypi>
2019-03-22 11:22:59 +0300

    Changing the servoce_stats func to filter output

--- piclock_web_modules.py
+++ piclock_web_modules.py
@@ -69,7 +69,7 @@ def service_control(service, action):
         return 'Only up/down are supported'
 
 def service_stats():
-    result = subprocess.run('systemctl status piclock_*.service',
+    result = subprocess.run('systemctl status piclock_*.service | grep "●\|Active"',
                             shell=True,
                             stdout=subprocess.PIPE)
     return result.stdout.decode('utf-8')
